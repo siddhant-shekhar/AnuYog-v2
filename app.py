efc9f33300bd54d0837bd0053336f21075a3e6be
Siddhant <104164130+siddhant-shekhar@users.noreply.github.com>
2023-04-16 06:42:59 +0000

    added answers to database

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, jsonify
-from database import add_counselor_to_db
+from database import add_counselor_to_db, add_answers_to_db
 
 app = Flask(__name__)
 
@@ -19,19 +19,25 @@ def counselor():
   return render_template("counselor.html")
 
 
-
 @app.route("/counselor/registered", methods=['post'])
 def register_as_counselor():
   data = request.form
   add_counselor_to_db(data)
-  return render_template('counselor_registered.html', 
-                         counselor=data,
-                         )
+  return render_template(
+    'counselor_registered.html',
+    counselor=data,
+  )
+
+
+@app.route("/questions/answers", methods=['post'])
+def answers():
+  data = request.form
+  add_answers_to_db(data)
+  return render_template('answers.html', answers=data)
 
 
 # @app.route("/api/counselor-registered")
 # def
-
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -35,3 +35,42 @@ def add_counselor_to_db(data):
             'email':data['email']
             }
     conn.execute(query, params)
+
+
+def add_answers_to_db(data):
+  with engine.connect() as conn:
+    query= text("""INSERT INTO questions_anuyog(question_1,question_2,question_3,question_4,question_5,question_6,question_7,question_8,question_9,question_10,question_11,question_12,question_13,question_14,question_15,question_16,question_17,question_18,question_19,question_20,question_21,question_22,question_23,question_24,question_25,question_26,question_27,question_28,question_29,question_30 )
+                        VALUES (:question_1,:question_2,:question_3,:question_4,:question_5,:question_6,:question_7,:question_8,:question_9,:question_10,:question_11,:question_12,:question_13,:question_14,:question_15,:question_16,:question_17,:question_18,:question_19,:question_20,:question_21,:question_22,:question_23,:question_24,:question_25,:question_26,:question_27,:question_28,:question_29,:question_30)""")
+    
+    params= {'question_1':data['question_1'],
+             'question_2':data['question_2'],
+             'question_3':data['question_3'],
+             'question_4':data['question_4'],
+             'question_5':data['question_5'],
+             'question_6':data['question_6'],
+             'question_7':data['question_7'],
+             'question_8':data['question_8'],
+             'question_9':data['question_9'],
+             'question_10':data['question_10'],
+             'question_11':data['question_11'],
+             'question_12':data['question_12'],
+             'question_13':data['question_13'],
+             'question_14':data['question_14'],
+             'question_15':data['question_15'],
+             'question_16':data['question_16'],
+             'question_17':data['question_17'],
+             'question_18':data['question_18'],
+             'question_19':data['question_19'],
+             'question_20':data['question_20'],
+             'question_21':data['question_21'],
+             'question_22':data['question_22'],
+             'question_23':data['question_23'],
+             'question_24':data['question_24'],
+             'question_25':data['question_25'],
+             'question_26':data['question_26'],
+             'question_27':data['question_27'],
+             'question_28':data['question_28'],
+             'question_29':data['question_29'],
+             'question_30':data['question_30']}
+
+    conn.execute(query, params)
